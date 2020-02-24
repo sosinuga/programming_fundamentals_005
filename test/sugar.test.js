88d16720c142e7a2092ed8ff76aeece4a8aad924
Sike Osinuga <adesikeoye@yahoo.com>
2020-02-24 22:36:08 +0000

    function has and function sum test passed

--- test/sugar.test.js
+++ test/sugar.test.js
@@ -136,7 +136,7 @@ describe.only("Object.get", () => {
   });
 });
 
-describe("Object.has", () => {
+describe.only("Object.has", () => {
   test("returns true if the object has the give key", () => {
     const obj1 = { name: "Sally", location: "Leeds" };
     expect(Object.has(obj1, "name")).toBe(true);
@@ -161,7 +161,7 @@ describe("Object.has", () => {
   });
 });
 
-describe("Object.sum", () => {
+describe.only("Object.sum", () => {
   test("returns the sum of all the values when all values are numbers", () => {
     const obj = {
       a: 1,
